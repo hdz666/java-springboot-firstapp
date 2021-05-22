10e5e500b31888ab681889cbcd54c7a18146a582
Hardik Maheshwari <swapnayhardik@gmail.com>
2021-05-22 19:52:44 +0530

    5th Commit
    Signed-off-by: Hardik Maheshwari <swapnayhardik@gmail.com>

---
+++ first-springboot-proj/src/main/java/com/hardik/springboot/app/jpa/UserCommandLineRunner.java
@@ -0,0 +1,46 @@
+package com.hardik.springboot.app.jpa;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserCommandLineRunner implements CommandLineRunner{
+
+	private static final Logger log = LoggerFactory.getLogger(UserCommandLineRunner.class);
+	
+	
+	@Autowired
+	private UserRepository H_repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+
+		H_repo.save(new User("Ranga", "Admin"));
+        H_repo.save(new User("Ravi", "User"));
+        H_repo.save(new User("Satish", "Admin"));
+        H_repo.save(new User("Raghu", "User"));
+		
+        for(User us : H_repo.findAll()) {
+        	log.info(us.toString());
+        }
+        
+        log.info("Admin Users are __________");
+        log.info("Admin Users are __________");
+        for(User user: H_repo.findByRole("Admin")) {
+        	log.info(user.toString());
+        }
+        
+		System.out.println("UserCommandLineRunner");
+		
+		
+		
+	}
+	
+
+}
